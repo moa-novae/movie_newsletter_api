dcb7b27f1e6db2281f0e4b72db7f1898bed78952
Pumpkin-spice-cloud <alan_mo@live.com>
2020-04-27 02:17:43 -0400

    Refactor: all pool config settings now reference .env

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,6 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "movienewsletter",
+  user: process.env.user,
   host: process.env.host,
   database: process.env.database,
   password: process.env.password,
